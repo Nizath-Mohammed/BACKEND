235817575452a7937bcd84ac91edb2f61cb6489d
Nizath-mohammed <Kit27.ad36@gmail.com>
2026-01-07 09:07:16 +0530

    react was added

---
+++ react-intro/index.jsx
@@ -0,0 +1,9 @@
+import {createRoot} from 'react-dom/client';
+let container = createRoot(document.getElementById('root'));
+
+container.render(
+    <div>   
+        <h1>Hello React!</h1>
+        <p>This is my first React Application.</p>
+    </div>
+)
